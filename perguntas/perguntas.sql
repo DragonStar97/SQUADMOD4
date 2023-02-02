a9339f17473924ae5b2fc3b16d0628de8ba840c4
Beatrice Macruz <113552366+BeatriceMacruz@users.noreply.github.com>
2023-02-02 16:56:04 -0300

    perguntas
    ...

---
+++ perguntas/perguntas.sql
@@ -0,0 +1,9 @@
+-- Quantas casas existem em Westeros? 
+-- Regiões com mais casas:
+-- Top 7 eps com maior duração:
+-- Top 7 eps com a melhor classificação:
+-- Top 7 atores com a maior participação por eps:
+-- Top 7 diretores que mais participaram da série:
+-- Top 7 eps com as melhores avaliçoes entre 2015 e 2019:
+
+select * from got_ep
