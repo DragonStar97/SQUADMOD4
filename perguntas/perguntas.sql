f5864e3b8fda318639a1ea2abd08e2930d464940
NHCardoso <113553259+NHCardoso@users.noreply.github.com>
2023-02-06 14:24:18 -0300

    Update perguntas.sql

--- perguntas/perguntas.sql
+++ perguntas/perguntas.sql
@@ -1,27 +1,27 @@
--- Quantas casas existem em Westeros? 
--- Regiões com mais casas:
--- Top 7 eps com maior duração:
--- Top 7 eps com a melhor classificação:
--- Top 7 atores com a maior participação por eps:
--- Top 7 diretores que mais participaram da série:
--- Top 7 eps com as melhores avaliçoes entre 2015 e 2019:
-
 select * from got_ep
 
+-- #1 Quantas casas existem em Westeros? 
 
--- Quantas casas existem em Westeros? 
 select count(House_name) from casas;
--- 471 casas 
--- Regiões com mais casas:
 
--- Top 7 eps com maior duração: 
+-- #2 Regiões com mais casas:
+
+
+-- #3 Top 7 eps com maior duração: 
+
 SELECT Title, Season, Episode, Duration FROM got_episodios ORDER BY Duration DESC limit 7;
--- Top 7 eps com a melhor classificação: 
+
+-- #4 Top 7 eps com a melhor classificação: 
+
 SELECT Title, Season, Episode, Rating FROM Episodes_appeared ORDER BY Rating DESC limit 7;
--- Top 7 atores com a maior participação por eps:
+
+-- #5 Top 7 atores com a maior participação por eps:
+
 SELECT actor, nome_personagem,Episodes_appeared  FROM personagens ORDER BY Episodes_appeared DESC limit 7;
 
--- Top 7 diretores que mais participaram da série:
--- Top 7 eps com as melhores avaliçoes entre 2015 e 2019:
+-- #6 Top 7 diretores que mais participaram da série:
+
+
+-- #7 Top 7 eps com as melhores avaliçoes entre 2015 e 2019:
 
 SELECT Actor/ess, nome_personagem FROM personagens ORDER BY Episodes_appeared DESC limit 7;
